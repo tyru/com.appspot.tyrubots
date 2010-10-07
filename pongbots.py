0fab65938fac95e8e7f82ed676bbbd54092ce559
tyru <tyru.exe@gmail.com>
2010-10-07 22:07:49 +0900

    now it works.

--- pongbots.py
+++ pongbots.py
@@ -6,29 +6,29 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
 import re
+from urlparse import urlparse
+from django.utils import simplejson
 
-import sys
-sys.path.append('lib')
-from gaeLingr import Lingr
 
 class PongBots(webapp.RequestHandler):
-    def get(self):
+    def get(self, path):
+        self.response.headers['Content-Type'] = 'text/plain'
         self.response.out.write("PongBots: hi")
-    def post(self):
-        lingr_id = 'pong_bots'
-        lingr_pass = 'h9WezlKz6GFow' # XXX: ???
-        lingr = Lingr(lingr_id, lingr_pass)
-        lingr.create_session()
+
+    def post(self, path):
         r = re.compile(r"^\s*ping\s*$", re.IGNORECASE)
-        for e in lingr.stream():
-            if 'message' in e and r.match(e['message']['text']):
-                # TODO: Sanitize
-                lingr.say('@' + e['message']['nickname'] + ' pong')
+        json = simplejson.loads(self.request.get('json'))
+        self.response.headers['Content-Type'] = 'text/plain'
+
+        for e in json['events']:
+            if r.match(e['message']['text']):
+                msg = e['message']['nickname'] + ': pong'
+                self.response.out.write(msg)
 
 
 
 if __name__ == "__main__":
     application = webapp.WSGIApplication([
-        ('/pongbots/.*', PongBots),
+        ('/pongbots/(.*)', PongBots),
     ], debug=True)
     run_wsgi_app(application)
